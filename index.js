f7d777c67427f14e1de5058743f9de88e9cb529a
outbounder <obiwonn@gmail.com>
2019-09-10 21:55:19 +0300

    upgrade impl to use organic-dna-repo-loader instead

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {exec} = require('child_process')
-const loadDna = require('organic-dna-loader')
+const loadDna = require('organic-dna-repo-loader')
 const path = require('path')
 const colors = require('chalk')
 const {forEach} = require('p-iteration')
@@ -21,7 +21,8 @@ const formatCellName = function (value) {
 
 module.exports = function (angel) {
   const CELLS_ROOT = angel.cells_root || process.cwd()
-  const executeCommand = async function ({ cellName, cmd, cwd, env, childHandler }) {
+
+  const executeCommand = async function ({ cellName, cmd, cwd, env, childHandler, forwardStdin }) {
     return new Promise((resolve, reject) => {
       console.log(formatCellName(cellName), cmd)
       let child = exec(cmd, {
@@ -37,57 +38,61 @@ module.exports = function (angel) {
       child.stderr.on('data', chunk => {
         console.error(formatCellName(cellName), colors.red(chunk.toString()))
       })
+      if (forwardStdin) {
+        process.stdin.pipe(child.stdin)
+        process.stdin.resume()
+      }
       child.on('close', status => {
         if (status !== 0) return reject(new Error(cellName + ' ' + cmd + ' returned ' + status))
         resolve()
       })
     })
   }
-  const executeCommandOnCells = async function ({cmd, cellName, groupName}) {
-    return new Promise((resolve, reject) => {
-      loadDna(path.join(CELLS_ROOT, 'dna'), async (err, dna) => {
-        if (err) return reject(err)
-        let tasks = []
-        let cells = cellsinfo(dna.cells)
-        cells.forEach((cell) => {
-          if (cellName && cell.name !== cellName) return
-          if (groupName && cell.groups.indexOf(groupName) === -1) return
-          tasks.push({
-            name: cell.name,
-            cellDna: cell.dna,
-            cwd: cell.dna.cwd
-          })
+  const executeCommandOnCells = async function ({cmd, cellName, groupName, forwardStdin}) {
+    return new Promise(async (resolve, reject) => {
+      let dna = await loadDna({root: CELLS_ROOT})
+      let tasks = []
+      let cells = cellsinfo(dna.cells)
+      cells.forEach((cell) => {
+        if (cellName && cell.name !== cellName) return
+        if (groupName && cell.groups.indexOf(groupName) === -1) return
+        tasks.push({
+          name: cell.name,
+          cellDna: cell.dna,
+          cwd: cell.dna.cwd
         })
-        if (tasks.length === 0) {
-          return reject(new Error('no cells found'))
-        }
-        let runningChilds = []
-        let childHandler = function (child) {
-          runningChilds.push(child)
-          child.on('close', () => {
-            runningChilds.splice(runningChilds.indexOf(child), 1)
-          })
-        }
-        forEach(tasks, async taskInfo => {
-          return executeCommand({
-            cellName: taskInfo.name,
-            cmd: cmd,
-            cwd: path.join(CELLS_ROOT, taskInfo.cwd),
-            env: process.env,
-            childHandler: childHandler
-          })
-        }).then(resolve).catch(async err => {
-          let pids = runningChilds.map(v => v.pid)
-          await forEach(pids, terminateAsync)
-          reject(err)
+      })
+      if (tasks.length === 0) {
+        return reject(new Error('no cells found'))
+      }
+      let runningChilds = []
+      let childHandler = function (child) {
+        runningChilds.push(child)
+        child.on('close', () => {
+          runningChilds.splice(runningChilds.indexOf(child), 1)
         })
+      }
+      forEach(tasks, async taskInfo => {
+        return executeCommand({
+          cellName: taskInfo.name,
+          cmd: cmd,
+          cwd: path.join(CELLS_ROOT, taskInfo.cwd),
+          env: process.env,
+          childHandler: childHandler,
+          forwardStdin: forwardStdin
+        })
+      }).then(resolve).catch(async err => {
+        let pids = runningChilds.map(v => v.pid)
+        await forEach(pids, terminateAsync)
+        reject(err)
       })
     })
   }
   angel.on(/repo cell (.*) -- (.*)/, function (angel, done) {
     executeCommandOnCells({
       cmd: angel.cmdData[2],
-      cellName: angel.cmdData[1]
+      cellName: angel.cmdData[1],
+      forwardStdin: true
     }).then(() => done()).catch(done)
   })
     .description('executes :cmd on cell by its :name')
@@ -107,23 +112,19 @@ module.exports = function (angel) {
   })
     .description('executes :cmd on all cells within group with :groupname')
     .example('repo cellgroup :groupname -- :cmd')
-  angel.on(/repo list cells/, function (angel, done) {
-    loadDna(path.join(CELLS_ROOT, 'dna'), async (err, dna) => {
-      if (err) throw err
-      let cells = cellsinfo(dna.cells)
-      for (let i = 0; i < cells.length; i++) {
-        console.info(`${cells[i].name} - ${cells[i].dna.cellKind} @ ${cells[i].dna.cwd}`)
-      }
-    })
+  angel.on(/repo list cells/, async function (angel, done) {
+    let dna = await loadDna({root: CELLS_ROOT})
+    let cells = cellsinfo(dna.cells)
+    for (let i = 0; i < cells.length; i++) {
+      console.info(`${cells[i].name} - ${cells[i].dna.cellKind} @ ${cells[i].dna.cwd}`)
+    }
   })
     .description('lists all found cells')
     .example('repo list cells')
-  angel.on(/repo dump cells.json/, function (angel, done) {
-    loadDna(path.join(CELLS_ROOT, 'dna'), async (err, dna) => {
-      if (err) throw err
-      let cells = cellsinfo(dna.cells)
-      console.info(JSON.stringify(cells))
-    })
+  angel.on(/repo dump cells.json/, async function (angel, done) {
+    let dna = await loadDna({root: CELLS_ROOT})
+    let cells = cellsinfo(dna.cells)
+    console.info(JSON.stringify(cells))
   })
     .description('dumps all found cells with their resolved DNA included as json')
     .example('repo dump cells.json')
